3b2924daf82427febe522495b10dd5aba37d3792
4FSB <frs.ksa@gmail.com>
2014-04-12 12:12:56 +0300

    add some important db info

--- adem.sql
+++ adem.sql
@@ -3,7 +3,6 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 11 أبريل 2014 الساعة 13:50
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -48,6 +47,15 @@ CREATE TABLE IF NOT EXISTS `settings` (
   PRIMARY KEY (`sid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
+
+INSERT INTO `settings` (`sid`, `sett_name`, `sett_value`) VALUES
+(1, 'site_url', ''),
+(2, 'site_name', 'أديم'),
+(3, 'site_descrip', 'سكربت للمواقع الشخصية والتعريفية .'),
+(4, 'google_code', ''),
+(5, 'email', ''),
+(6, 'password', '8a8e83876e81242bee8a620b2967dc33');
+-- Password : adem
 -- --------------------------------------------------------
 
 --
@@ -60,6 +68,16 @@ CREATE TABLE IF NOT EXISTS `social` (
   `sc_link` varchar(255) NOT NULL,
   PRIMARY KEY (`sc_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+INSERT INTO `social` (`sc_id`, `sc_name`, `sc_link`) VALUES
+(1, 'twitter', 'http://Twitter.com'),
+(2, 'facebook', 'http://Facebook.com'),
+(3, 'google-plus', 'http://plus.google.com'),
+(4, 'instagram', 'http://instagram.com'),
+(5, 'tumblr', 'http://tumblr.com'),
+(6, 'skype', 'http://skype.com'),
+(7, 'youtube', 'http://youtube.com'),
+(8, 'github', 'http://github.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
