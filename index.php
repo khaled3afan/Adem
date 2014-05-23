d6b99210ad46b129f8f86c9dade48a48771769f5
4FSB <frs.ksa@gmail.com>
2014-05-24 00:36:04 +0300

    Fixed exploit , bugs , run visitor query

--- index.php
+++ index.php
@@ -1,47 +1,25 @@
 <?php
 include './ad-define.php';
 
+$view = new AD_View(THEME_NAME);
+$db = new AD_Query;
 
-if(isset($_GET['p'])){
-    if(!empty($_GET['p'])){
-        $file = $_GET['p'].".php";
-        if(is_file($file) && file_exists($file)){
-            include $file;
-        }else{
-            $view = new AD_View(THEME_NAME);
-            $view->view("404",array(
-                "key"=>array(),
-                "title"=>"خطأ ، تعذر إيجاد الصفحة.",
-                "Des"=>"",
-            ));
-        }
-    }else{
-        $view = new AD_View(THEME_NAME);
-        $view->view("404",array(
-            "key"=>array(),
-            "title"=>"خطأ ، تعذر إيجاد الصفحة.",
-            "Des"=>"",
-        ));
-    }
-}else{
-    
-    $view = new AD_View(THEME_NAME);
-    $db = new AD_Query;
-    
-    /* get INFO from DB */
-    $info_query = "SELECT * FROM info ORDER BY `info_id` ASC  LIMIT 0,10";
-    $info = $db->DB_Query($info_query);
-    
-    /* get SOCIAL from DB */
-    $social_query = "SELECT * FROM social WHERE sc_link !='' limit 0,10";
-    $social = $db->DB_Query($social_query);
-    
-    // View
-    $view->view("index",array(
-        "key"=>array(),
-        "title"=>"index",
-        "Des"=>"",
-        "social"=>$social,// send queries to view files
-        "info"=>$info
-    ));
-}
+/* get INFO from DB */
+$info_query = "SELECT * FROM info ORDER BY `info_id` ASC  LIMIT 0,10";
+$info = $db->DB_Query($info_query);
+
+/* get SOCIAL from DB */
+$social_query = "SELECT * FROM social WHERE sc_link !='' limit 0,10";
+$social = $db->DB_Query($social_query);
+
+/* UPDATE VISITE */
+$db->DB_Query("UPDATE settings SET sett_value=sett_value+1 WHERE sett_name='visits'");
+// View
+$view->view("index",array(
+	"key"=>array(),
+	"title"=>"index",
+	"Des"=>"",
+	// send queries to view files
+	"social"=>$social,
+	"info"=>$info
+));
